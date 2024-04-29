65aa8dc617ba8905890b1d8fdb8c5b48088b7cdd
martincodesthings <gayinstem@gmail.com>
2024-04-28 21:59:42 -0500

    add loading indicator

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -57,7 +57,7 @@ function Form({ route, method }) {
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
             />
-            
+            {loading && <LoadingIndicator />}
             <button className="form-button" type="submit">
                 {name}
             </button>
