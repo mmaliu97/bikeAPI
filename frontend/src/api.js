7f0eb754676bf40cb7d355aeb76e67b69b18e4b9
martincodesthings <gayinstem@gmail.com>
2024-05-27 15:47:24 -0500

    add new backend service url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/bikeapi/backend/rest-api-be2/v1.0";
+const apiUrl = "https://c7889c0d-220d-4dd0-b64a-605971ef4c3d-dev.e1-us-cdp-2.choreoapis.dev/bikeapi/backend/rest-api-be2/v1.0";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
