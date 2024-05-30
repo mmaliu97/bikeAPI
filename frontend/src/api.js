c0a63cec1877af1e4d4bd903d7f96bcedb48c241
martincodesthings <gayinstem@gmail.com>
2024-05-30 17:53:18 -0500

    changed apiurl connection

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreoapis.dev/bikeapi/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/bike-api-2/backend-ixj/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
